42792d53f8f398ce9edaf0e2cce366b5040524de
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-07-21 21:37:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1849,10 +1849,13 @@ st.image("k7.jpg", use_container_width=True)
 st.image("k10.jpg", use_container_width=True)
 st.image("k8.jpg", use_container_width=True)
 st.sidebar.image("k4.jpg", use_container_width=True)
-st.sidebar.image("k15.jpg", use_container_width=True)
+
 st.image("k16.jpg", use_container_width=True)
 st.image("k13.jpg", use_container_width=True)
 st.image("k14.jpg", use_container_width=True)
+st.sidebar.image("k16.jpg", use_container_width=True)
+st.image("k17.jpg", use_container_width=True)
+
 
 st.markdown("---")
 st.markdown("""
