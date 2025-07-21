c8842b98eeb05cdea182c165840a9a670070e85e
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-07-21 13:54:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ st.set_page_config(
 )
 
 
-st.image("k2.jpg", caption="So dreamy~ 💫", use_column_width=True)
+st.image("k2.jpg", use_column_width=True)
+st.sidebar.image("k1.jpg", caption="✨ In the dreamy sidebar ✨", use_column_width=True)
+
 
 # Initialize session state
 if 'images' not in st.session_state:
