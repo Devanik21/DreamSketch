0be169117c427ece016aea05f26036935f62a794
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-10-21 11:35:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -707,7 +707,7 @@ st.markdown("""
 
 # --- SET BACKGROUND IMAGE ---
 try:
-    base64_img = get_base64_of_bin_file('k_bg_1.jpg')
+    base64_img = get_base64_of_bin_file('bg_2.jpg')
     bg_css = f'''
     <style>
     .stApp {{
