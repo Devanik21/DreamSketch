0089e2fe24381b61ff34785b6e5e5949119bd9e4
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-10-21 12:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -776,7 +776,7 @@ st.markdown("""
 
 # --- SET BACKGROUND IMAGE ---
 try:
-    base64_img = get_base64_of_bin_file('Gemini_Generated_Image_gx24iygx24iygx24.png')
+    base64_img = get_base64_of_bin_file('fantasy-eye-illustration.jpg')
     bg_css = f'''
     <style>
     .stApp {{
